f78c029fbadf08bc98b50aa3c2317a9693680171
Kuda Nyamupa <nyamupakuda@gmail.com>
2026-01-07 15:40:26 +0200

    Expanded __init__ and added __str__ and __len__ methods

--- Sequencer.py
+++ Sequencer.py
@@ -1,6 +1,13 @@
 class Sequencer:
-    def __init__(self, PID):
-        self.pid = PID
+    def __init__(self, pid, sequence, refgenome, organism, technology):
+        self.pid = pid
+        self.sequence = sequence
+        self.refgenome = refgenome
+        self.organism = organism
+        self.technology = technology
     
     def __str__(self):
-        pass
+        return f"Patient ID: {self.pid} /n Length: {len(self.sequence)} bp /n Organism: {self.organism}"
+    
+    def __len__(self):
+        return len(self.sequence)
