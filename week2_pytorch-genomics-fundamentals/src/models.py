f7cca8588a35bcf3379d142ade79875fdeceb003
Kuda Nyamupa <nyamupakuda@gmail.com>
2026-02-03 15:54:52 +0200

    Working on CNN and making current synthetic data more realistic.

--- week2_pytorch-genomics-fundamentals/src/models.py
+++ week2_pytorch-genomics-fundamentals/src/models.py
@@ -2,15 +2,16 @@ import torch
 import torch.nn as nn
 
 class SimpleDNAClassifier(nn.Module):
-    def __init__(self, input_size: int, hidden_size: int, output_size: int):
+    def __init__(self, input_size: int, hidden_size: int, output_size: int, seq_length: int):
         super(SimpleDNAClassifier, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc1 = nn.Conv1d(in_channels=input_size, out_channels=hidden_size, kernel_size=3, padding=1)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.fc2 = nn.Linear(hidden_size * seq_length, output_size)
         self.pred = nn.Sigmoid()
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.fc1(x)
         out = self.relu(out)
+        out = torch.flatten(out, start_dim=1)
         out = self.fc2(out)
         out = self.pred(out)
         return out
