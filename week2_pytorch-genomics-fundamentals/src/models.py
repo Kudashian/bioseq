f05e42a4aff626d024c79bb3a25120ad5d98c283
Kuda Nyamupa <nyamupakuda@gmail.com>
2026-02-09 17:43:46 +0200

    Update to DNA classifier

--- week2_pytorch-genomics-fundamentals/src/models.py
+++ week2_pytorch-genomics-fundamentals/src/models.py
@@ -6,12 +6,16 @@ class SimpleDNAClassifier(nn.Module):
         super(SimpleDNAClassifier, self).__init__()
         self.fc1 = nn.Conv1d(in_channels=input_size, out_channels=hidden_size, kernel_size=3, padding=1)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size * seq_length, output_size)
+        self.fc2 = nn.Conv1d(in_channels=hidden_size, out_channels=hidden_size*2, kernel_size=3, padding=1)
+        self.relu = nn.ReLU()
+        self.fc3 = nn.Linear(hidden_size * 2 * seq_length, output_size)
         self.pred = nn.Sigmoid()
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.fc1(x)
         out = self.relu(out)
-        out = torch.flatten(out, start_dim=1)
         out = self.fc2(out)
+        out = self.relu(out)
+        out = out.flatten(start_dim=1)
+        out = self.fc3(out)
         out = self.pred(out)
         return out
