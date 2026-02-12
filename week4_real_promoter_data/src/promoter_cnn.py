2d927ee53f41a7c98fdb9e6fcec612d27a6c1689
Kuda Nyamupa <nyamupakuda@gmail.com>
2026-02-12 20:00:41 +0200

    week 4: Building CNN with real promoter data.

---
+++ week4_real_promoter_data/src/promoter_cnn.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+
+class PromoterCNNClassifier(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.conv1 = nn.Conv1d(input_size, hidden_size, kernel_size=10, padding=5)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv1d(hidden_size, hidden_size * 2, kernel_size=10, padding=5)
+        self.global_pool = nn.AdaptiveMaxPool1d(1)
+        self.dropout = nn.Dropout(0.5)
+        self.fc = nn.Linear(hidden_size * 2, output_size)
+        self.pred = nn.Sigmoid()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = self.conv1(x)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.relu(out)
+        out = self.global_pool(out)
+        out = out.squeeze(-1)  # Remove the last dimension
+        out = self.dropout(out)
+        out = self.fc(out)
+        out = self.pred(out)
+        return out
